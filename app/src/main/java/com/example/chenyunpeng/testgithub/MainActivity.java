5cd68c481db2fef022bc3b8e69c0318af222257a
chenyunpeng <1036029770@qq.com>
2016-09-05 18:46:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenyunpeng/testgithub/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.chenyunpeng.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void click(View view){
+        Toast.makeText(this,"zheshiyigeceshidemo",Toast.LENGTH_SHORT).show();
+    }
+}
